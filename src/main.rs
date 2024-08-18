63ee2b381660b3102a060e75407a8bd1c496389d
marcelldac <devmarcell@protonmail.com>
2024-08-18 00:03:48 -0300

    feat: separate and show tokens

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,111 @@
 use std::{/* fs, */ env, fs};
 
 #[derive(Debug)]
+enum TokenKind {
+    Identifier,
+    Assign,
+    Let,
+    String,
+}
+
+#[derive(Debug)]
+struct Token {
+    kind: TokenKind,
+    literal: String,
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, literal: String) -> Self {
+        Self { kind, literal }
+    }
+}
+
+#[derive(Debug)]
 struct Lexer {
-    contents: String,
+    source: Vec<char>,
+    counter: usize,
 }
 
 impl Lexer {
     pub fn new(contents: String) -> Self {
-        Self { contents }
+        Self {
+            source: contents.chars().collect(),
+            counter: 0,
+        }
+    }
+
+    pub fn lex(&mut self) {
+        let mut tokens = Vec::<Token>::new();
+
+        while self.source.len() > self.counter {
+            let c = self.current_char();
+
+            match c {
+                '=' => {
+                    tokens.push(Token::new(TokenKind::Assign, "=".to_owned()));
+                    self.counter += 1
+                }
+                '\'' => {
+                    self.counter += 1;
+
+                    let mut buffer = String::new();
+
+                    while self.current_char() != '\'' {
+                        buffer.push(self.current_char());
+                        self.counter += 1;
+                    }
+
+                    self.counter += 1;
+
+                    tokens.push(Token::new(TokenKind::String, buffer));
+                }
+                _ if c.is_alphabetic() => {
+                    let mut buffer = String::new();
+                    buffer.push(c);
+                    self.counter += 1;
+
+                    while self.current_char().is_alphabetic() {
+                        buffer.push(self.current_char());
+                        self.counter += 1;
+                    }
+
+                    let kind: TokenKind = match buffer.as_str() {
+                        "let" => TokenKind::Let,
+                        _ => TokenKind::Identifier,
+                    };
+
+                    println!("{:?}", kind);
+                }
+                _ => self.counter += 1,
+            }
+        }
+
+        print!("{:?}", tokens);
+    }
+
+    fn current_char(&self) -> char {
+        *self.source.get(self.counter).unwrap()
     }
 }
 
 fn main() {
-    let file = env::args().nth(1).unwrap();
-    let contents = fs::read_to_string(file).unwrap();
+    let maybe_file = env::args().nth(1);
 
-    let lexer = Lexer::new(contents);
+    let file = if let Some(f) = maybe_file {
+        f
+    } else {
+        panic!("Expected a file.");
+    };
 
-    println!("{:?}", lexer);
+    let maybe_contents = fs::read_to_string(file);
+
+    let contents = if maybe_contents.is_ok() {
+        maybe_contents.unwrap()
+    } else {
+        panic!("Could not open file for reading.");
+    };
+
+    let mut lexer = Lexer::new(contents);
+
+    lexer.lex();
 }
