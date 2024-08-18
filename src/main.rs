eaf885bd7d4e87bd3b63353f6a28e0d57c1e9645
marcelldac <devmarcell@protonmail.com>
2024-08-17 22:32:36 -0300

    feat: create lexer implementation and struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use std::{/* fs, */ env, fs};
 
+#[derive(Debug)]
+struct Lexer {
+    contents: String,
+}
+
+impl Lexer {
+    pub fn new(contents: String) -> Self {
+        Self { contents }
+    }
+}
+
 fn main() {
     let file = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(file).unwrap();
-    println!("{:?}", contents);
+
+    let lexer = Lexer::new(contents);
+
+    println!("{:?}", lexer);
 }
